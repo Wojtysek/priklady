f93a7a3dd49a53d46f56162c6bd6e865fee2f27a
Wojtysek <vojtahr1@post.cz>
2022-05-23 13:12:15 +0200

    Přidejte soubory projektu.

---
+++ priklady/priklady.cpp
@@ -0,0 +1,18 @@
+﻿#include <stdio.h>
+
+//precist zadany retezec a napsat kolikrat se v nem vyskytuje urcene pismeno pismeno:
+int main(void) {
+
+	char vstup[1000], znak;
+	int pocet = 0;
+	printf("vlozte retezec: ");
+	//vstup textu i s mezerami o max. velikosti bufferu
+	fgets(vstup, sizeof(vstup), stdin);
+	printf("vlozte hledane pismeno: ");
+	scanf_s("%c", &znak);
+
+	for (int i = 0; vstup[i] != '\0'; i++) {
+		if (vstup[i] == znak) pocet++;
+	}
+	printf("pocet pismen v %c byl %d\n", znak, pocet);
+}
