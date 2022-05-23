717e6e31f6a367c880fc87bfbcb2869c6781eb65
Wojtysek <vojtahr1@post.cz>
2022-05-23 13:59:16 +0200

    update kamo

--- priklady/priklady.cpp
+++ priklady/priklady.cpp
@@ -4,15 +4,54 @@
 int main(void) {
 
 	char vstup[1000], znak;
-	int pocet = 0;
+	int znaky = 0;
+	int samohlasky = 0;
+	int mezery = 0;
+	int souhlasky = 0;
+	int cislice = 0;
+	int abc = 0;
 	printf("vlozte retezec: ");
 	//vstup textu i s mezerami o max. velikosti bufferu
 	fgets(vstup, sizeof(vstup), stdin);
 	printf("vlozte hledane pismeno: ");
 	scanf_s("%c", &znak);
 
+	//pocet hledanych pismen
 	for (int i = 0; vstup[i] != '\0'; i++) {
-		if (vstup[i] == znak) pocet++;
+		if (vstup[i] == znak) znaky++;
 	}
-	printf("pocet pismen v %c byl %d\n", znak, pocet);
+	printf("pocet %c je %d\n", znak, znaky);
+
+	//pocet samohlasek
+	for (int i = 0; vstup[i] != '\0';i++) {
+		if (vstup[i] == 'a'|| vstup[i] == 'e'|| vstup[i] == 'i'|| vstup[i] == 'o'|| vstup[i] == 'u') samohlasky++;
+	}
+	printf("pocet samohlasek: %d\n", samohlasky);
+
+	//pocet mezer
+	for (int i = 0; vstup[i] != '\0'; i++) {
+		if (vstup[i] == ' ') mezery++;
+	}
+	printf("pocet mezer v textu: %d\n", mezery);
+
+	//pocet tvrdych souhlasek
+	for (int i = 0; vstup[i] != '\0'; i++) {
+		if (vstup[i] == 'h' || vstup[i] == 'ch' || vstup[i] == 'k' || vstup[i] == 'r' || vstup[i] == 'd' || vstup[i] == 't' || vstup[i] == 'n') souhlasky++;
+	}
+	printf("pocet tvrdych souhlasek: %d\n", souhlasky);
+
+	//pocet cislic
+	for (int i = 0; vstup[i] != '\0'; i++) {
+		if (vstup[i] >= 0 && vstup[i] <= 9) cislice++;
+	}
+	printf("pocet cislic: %d\n", cislice);
+
+	//pocet pismen z abecedy
+	for (int i = 0; vstup[i] != '\0'; i++) {
+		if (vstup[i] >= 'a' && vstup[i] <= 'z') abc++;
+	}
+	printf("pocet pismen abecedy: %d\n", abc);
+
+	//pocet stejnych znaku
+	
 }
